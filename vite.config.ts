568d6858ecc34fa3c5496a31a8d5e1e8152038a7
MKO <118934540+SDT413@users.noreply.github.com>
2025-10-29 06:52:46 +0100

    gh-pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/visualization_opendb_task/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/opendb_visualization/' : '/',
 })
